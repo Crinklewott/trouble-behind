27150e12692642916f29a64677e66c7ed623fa71
thingywhat <thingywhat@live.com>
2015-02-26 00:03:56 -0700

    Initial commit of a potential text adventure-ish thing

---
+++ trouble-behind.lisp
@@ -0,0 +1,90 @@
+;; State
+(defparameter *player-location* 'your-bedroom
+  "The current location of the player")
+
+(defparameter *map*
+  (with-open-file (nodes "map.lmap" :direction :input)
+    (read nodes))
+  "The map that contains all of the location nodes as well as the
+  edges that connect them.")
+
+;; Dynamically loads things from the map
+(defmacro defmap (entry getter-name fetcher-name)
+  "Defines a get-all-<things> function to fetch the alist for some
+specific thing from the loaded map and get-<things> to fetch an
+individual item"
+  `(let ((,entry (cadr (assoc ',entry *map*))))
+     (defun ,fetcher-name (key)
+       (cdr (assoc key ,entry)))
+     (defun ,getter-name ()
+       ,entry)))
+
+;; Load each type of map item we care about and creates a whole mess
+;; of utility functions
+(defmap nodes get-nodes get-node)
+(defmap edges get-all-edges get-edges)
+(defmap items get-items get-item)
+(defmap item-location-details get-all-item-details get-item-details)
+
+;; Grammar and string output functions
+(let ((vowels '(#\a #\e #\i #\o #\u #\y)))
+  (defun begins-with-vowel (thing)
+    "Returns the vowel a word begins with, if it begins with it. nil
+otherwise."
+    (find (char-downcase (car (coerce thing 'list))) vowels)))
+
+(defun stylize-string (str)
+  "Stylizes a string so it prints with correct capitalization and
+formatting from a string."
+  (labels ((style (char-list caps)
+	     (when char-list
+	       (let ((first (car char-list))
+		     (rest (cdr char-list)))
+		 (cond
+		   ((char= first #\space) (cons #\space (style rest caps)))
+		   ((char= first #\.) (cons #\. (style rest t)))
+		   (caps (cons (char-upcase first) (style rest nil)))
+		   (t (cons (char-downcase first) (style rest nil))))))))
+    (coerce (style (coerce (string-trim "()" str) 'list) t) 'string)))
+
+(defun stylize-list (list)
+  "Stylizes a list as a pretty string."
+  (stylize-string (prin1-to-string list)))
+
+;; Game description functions
+(defun describe-path (edge)
+  "Describes a path connecting two nodes."
+  (let ((place (caddr edge))
+	(direction (cadr edge)))
+    (let ((a/an (if (begins-with-vowel (prin1-to-string place)) 'an 'a)))
+  (list 'there 'is a/an place 'to 'the direction 'of 'here.))))
+
+(defun describe-edges (edges)
+  "Describes all of the exits from the current node."
+  (mapcan #'describe-path edges))
+
+(defun describe-items-at-location (item-details location)
+  "Describes the items at the passed-in location."
+  (mapcan (lambda (item)
+	    (copy-list
+	     (when (eq location (caadr item))
+	       (cadadr item))))
+	  item-details))
+
+;; Player location-oriented functions
+(defun look ()
+  "Outputs what the player sees around them."
+  (append
+   (describe-node (get-node *player-location*))
+   (describe-edges (get-edges *player-location*))
+   (describe-items-at-location (get-all-item-details) *player-location*)))
+
+(defun can-see (item)
+  "Checks if you can see an item currently"
+  (eq *player-location* (caar (get-item-details item))))
+
+(defun look-at (item)
+  "Gets the player to look at an item if they can see it."
+  (if (can-see item)
+      (describe-item (get-item item))
+      `(you cannot see any ,item around here.)))
