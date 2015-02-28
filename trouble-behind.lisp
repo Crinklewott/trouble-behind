ec32f7e164ddd273546f0a3ea34be6d8602994fa
thingywhat <thingywhat@live.com>
2015-02-27 23:09:36 -0700

    Added item location state, and more functional means to do stuff
    - New *item-locations*
    - New a/an grammar convenience function
    - Item location querying with item-location
    - Checking to see if an item has been moved with item-moved
    - describe-items-at-location, can-see, and look-at consider item state

--- trouble-behind.lisp
+++ trouble-behind.lisp
@@ -8,6 +8,11 @@
   "The map that contains all of the location nodes as well as the
   edges that connect them.")
 
+(defparameter *item-locations* 
+  (mapcar (lambda (x) (cons (car x) (caadr x)))
+	  (cadr (assoc 'item-location-details *map*)))
+  "An alist containing the locations of items")
+
 ;; Dynamically loads things from the map
 (defmacro defmap (entry getter-name fetcher-name)
   "Defines a get-all-<things> function to fetch the alist for some
@@ -31,7 +36,10 @@ individual item"
   (defun begins-with-vowel (thing)
     "Returns the vowel a word begins with, if it begins with it. nil
 otherwise."
-    (find (char-downcase (car (coerce thing 'list))) vowels)))
+    (find (char-downcase (car (coerce thing 'list))) vowels))
+  (defun a/an (thing)
+    "Returns a or an depending on the grammar used to describe something."
+    (if (begins-with-vowel (prin1-to-string thing)) 'an 'a)))
 
 (defun stylize-string (str)
   "Stylizes a string so it prints with correct capitalization and
@@ -51,13 +59,24 @@ formatting from a string."
   "Stylizes a list as a pretty string."
   (stylize-string (prin1-to-string list)))
 
+;; Querying
+(defun item-location (item)
+  "Fetches the location of an item"
+  (cdr (assoc item *item-locations*)))
+
+(defun item-moved (item)
+  "Returns t if an item has moved from its original location, nil
+otherwise."
+  (loop for i in *item-locations*
+     when (eq item (car i)) collect i into acc
+     when (< 1 (length acc)) return t))
+
 ;; Game description functions
 (defun describe-path (edge)
   "Describes a path connecting two nodes."
   (let ((place (caddr edge))
 	(direction (cadr edge)))
-    (let ((a/an (if (begins-with-vowel (prin1-to-string place)) 'an 'a)))
-  (list 'there 'is a/an place 'to 'the direction 'of 'here.))))
+    (list 'there 'is (a/an place) place 'to 'the direction 'of 'here.)))
 
 (defun describe-edges (edges)
   "Describes all of the exits from the current node."
@@ -65,11 +84,26 @@ formatting from a string."
 
 (defun describe-items-at-location (item-details location)
   "Describes the items at the passed-in location."
-  (mapcan (lambda (item)
+  (mapcan (lambda (item-detail)
 	    (copy-list
-	     (when (eq location (caadr item))
-	       (cadadr item))))
+	     (let ((item (car item-detail)))
+	       (when (eq location (item-location item))
+		 (if (item-moved item)
+		     `(there is ,(a/an item) ,item on the floor here.)
+		     (cadadr item-detail))))))
 	  item-details))
+
+(defun can-see (item location)
+  "Checks if you can see an item currently"
+  (let ((loc (item-location item)))
+    (or (eq location loc)
+	(eq 'inventory loc))))
+
+(defun look-at (item location)
+  "Gets the player to look at an item if they can see it."
+  (if (can-see item location)
+      (describe-item (get-item item))
+      `(you cannot see any ,item around here.)))
 
 ;; Player location-oriented functions
 (defun look ()
@@ -78,13 +112,3 @@ formatting from a string."
    (describe-node (get-node *player-location*))
    (describe-edges (get-edges *player-location*))
    (describe-items-at-location (get-all-item-details) *player-location*)))
-
-(defun can-see (item)
-  "Checks if you can see an item currently"
-  (eq *player-location* (caar (get-item-details item))))
-
-(defun look-at (item)
-  "Gets the player to look at an item if they can see it."
-  (if (can-see item)
-      (describe-item (get-item item))
-      `(you cannot see any ,item around here.)))
