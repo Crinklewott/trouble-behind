b69dc54ce2da560a4f02d83d3a65ab63a28f506e
thingywhat <thingywhat@live.com>
2015-03-01 21:19:49 -0700

    Added new useless word that will likely be used in plain english

--- trouble-behind.lisp
+++ trouble-behind.lisp
@@ -49,7 +49,7 @@ individual item"
 (defun fluff-word-p (word)
   "Returns if the passed in word is a fluff word and should be
 ignored."
-  (member word '(the at to my a can is)))
+  (member word '(the at to my a can is out)))
 
 (let ((vowels '(#\a #\e #\i #\o #\u #\y)))
   (defun begins-with-vowel (thing)
